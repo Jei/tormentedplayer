8b6413283bb3338d172602e39fb7e57405dd28d7
Andrea Jonus <j.jonus.j@gmail.com>
2020-03-18 17:01:06 +1100

    feat: change home page layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,51 +30,81 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder<FullAudioPlaybackState>(
-              stream: _player.fullPlaybackStateStream,
-              builder: (context, snapshot) {
-                final state = snapshot.data;
-                final metadata = snapshot.data.icyMetadata;
-
-                return Text(metadata != null && metadata.info.title != null ? metadata.info.title : "UNKNOWN");
-              }
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Image.network(
+                  'https://cataas.com/c',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            StreamBuilder<FullAudioPlaybackState>(
-                stream: _player.fullPlaybackStateStream,
-                builder: (context, snapshot) {
-                  final fullState = snapshot.data;
-                  final state = fullState?.state;
+          ),
+          StreamBuilder<IcyMetadata>(
+              stream: _player.icyMetadataStream,
+              builder: (context, snapshot) {
+                final String title = snapshot.data?.info?.title;
+                final List<String> parsedTitle = parseTitle(title);
 
-                  if (state == AudioPlaybackState.connecting ||
-                      state == AudioPlaybackState.none ||
-                      state == AudioPlaybackState.completed) {
-                    return CircularProgressIndicator();
-                  } else {
-                    bool isConnected = state == AudioPlaybackState.playing;
-                    return FloatingActionButton(
-                        child:
-                            Icon(isConnected ? Icons.stop : Icons.play_arrow),
-                        onPressed: () async {
-                          try {
-                            if (isConnected) {
-                              await _player.stop();
-                            } else {
-                              await _player.play();
-                            }
-                          } catch (err) {
-                            print(err);
-                            _player.stop();
-                          }
-                        });
-                  }
-                }),
-          ],
-        ),
+                return Column(
+                  children: <Widget>[
+                    Text(
+                      parsedTitle[1],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(parsedTitle[0]),
+                  ],
+                );
+              }),
+          StreamBuilder<AudioPlaybackState>(
+              stream: _player.playbackStateStream,
+              builder: (context, snapshot) {
+                final AudioPlaybackState state = snapshot.data;
+                final bool isLoading = state == AudioPlaybackState.connecting ||
+                    state == AudioPlaybackState.none ||
+                    state == AudioPlaybackState.completed;
+                final bool isConnected = state == AudioPlaybackState.playing;
+
+                return FloatingActionButton(
+                  child: isLoading
+                      ? SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.black),
+                          ))
+                      : Icon(isConnected ? Icons.stop : Icons.play_arrow),
+                  onPressed: () async {
+                    try {
+                      isConnected ? await _player.stop() : await _player.play();
+                    } catch (err) {
+                      print(err);
+                      _player.stop();
+                    }
+                  },
+                );
+              }),
+        ],
       ),
     );
+  }
+
+  static List<String> parseTitle(String title) {
+    if (title == null) return ['-', '-'];
+    final RegExp matcher = RegExp(r'^(.*) - (.*)$');
+    final match = matcher.firstMatch(title);
+
+    if (match == null) return ['-', '-'];
+
+    final song = match.group(1) ?? '-';
+    final artist = match.group(2) ?? '-';
+
+    return [song, artist];
   }
 }
