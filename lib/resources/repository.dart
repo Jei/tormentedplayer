aa519d04bba6d98d5f740fb82e26d3bf47ff6cf5
Andrea Jonus <j.jonus.j@gmail.com>
2020-03-30 02:00:45 +1100

    feat: add method to get tracks history to Repository

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -1,10 +1,12 @@
 import 'package:tormentedplayer/models/track.dart';
 import 'package:tormentedplayer/services/lastfm.dart' as lfm;
+import 'package:tormentedplayer/services/tormentedradio.dart' as tr;
 
 class Repository {
   final lfm.LastFM _lastFM = lfm.LastFM(lfm.LastFMConfig(
     apiKey: 'XXX', // TODO get the LastFM api key from a remote source
   ));
+  final tr.TormentedRadio _tormentedRadio = tr.TormentedRadio();
 
   Track _trackFromLastFMTrack(lfm.Track lfmTrack) {
     return Track(
@@ -15,9 +17,25 @@ class Repository {
     );
   }
 
+  Track _trackFromTRTrack(tr.Track trTrack) {
+    return Track(
+      title: trTrack?.name,
+      artist: trTrack?.artist,
+    );
+  }
+
   Future<Track> fetchTrack(String title, String artist) async {
     lfm.Track lfmTrack =
         await _lastFM.getTrackInfo(track: title, artist: artist);
     return _trackFromLastFMTrack(lfmTrack);
+  }
+
+  Future<List<Track>> fetchHistory() async {
+    tr.History trHistory = await _tormentedRadio.getHistory();
+
+    return [
+      _trackFromTRTrack(trHistory.current),
+      ...trHistory.previous.map(_trackFromTRTrack),
+    ];
   }
 }
