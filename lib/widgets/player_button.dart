1f795928934fb981aa46e328d32c3c9d85e0a499
Andrea Jonus <j.jonus.j@gmail.com>
2020-04-08 19:30:08 +1000

    refactor: improve the player's button style

--- lib/widgets/player_button.dart
+++ lib/widgets/player_button.dart
@@ -7,31 +7,52 @@ class PlayerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RadioBloc _bloc = Provider.of<RadioBloc>(context);
+    final color = Theme.of(context).primaryColorDark;
+    final highlight = Theme.of(context).primaryColor.withAlpha(30);
+    final splash = Theme.of(context).primaryColor.withAlpha(50);
 
-    return StreamBuilder<RadioPlaybackState>(
-      initialData: RadioPlaybackState.none,
-      stream: _bloc.playbackStateStream,
-      builder: (context, snapshot) {
-        final state = snapshot.data ?? RadioPlaybackState.none;
-        final bool isLoading = state == RadioPlaybackState.connecting;
-        final bool isPlaying = state == RadioPlaybackState.playing;
+    return Material(
+      child: Center(
+        child: Ink(
+          height: 72,
+          width: 72,
+          decoration: BoxDecoration(
+            border: Border.all(color: color, width: 1.0),
+            color: Colors.transparent,
+            shape: BoxShape.circle,
+          ),
+          child: StreamBuilder<RadioPlaybackState>(
+              initialData: RadioPlaybackState.none,
+              stream: _bloc.playbackStateStream,
+              builder: (context, snapshot) {
+                final state = snapshot.data ?? RadioPlaybackState.none;
+                final bool isLoading = state == RadioPlaybackState.connecting;
+                final bool isPlaying = state == RadioPlaybackState.playing;
 
-        return FloatingActionButton(
-          child: isLoading
-              ? SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                  ))
-              : Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-          onPressed: () {
-            if (isLoading) return;
+                return InkWell(
+                  splashColor: splash,
+                  highlightColor: highlight,
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: isLoading
+                      ? Padding(
+                          padding: EdgeInsets.all(16),
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(color),
+                          ))
+                      : Icon(
+                          isPlaying ? Icons.pause : Icons.play_arrow,
+                          color: color,
+                          size: 60,
+                        ),
+                  onTap: () {
+                    if (isLoading) return;
 
-            isPlaying ? _bloc.stopRadio() : _bloc.startRadio();
-          },
-        );
-      },
+                    isPlaying ? _bloc.stopRadio() : _bloc.startRadio();
+                  },
+                );
+              }),
+        ),
+      ),
     );
   }
 }
