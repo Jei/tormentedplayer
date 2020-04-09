2a328957cc2b79033ce679f842af065e06993594
Andrea Jonus <j.jonus.j@gmail.com>
2020-04-09 13:45:32 +1000

    refactor: bring back the TormentedRadio service to improve API calls

---
+++ lib/services/tormentedradio.dart
@@ -0,0 +1,114 @@
+import 'package:http/http.dart';
+import 'package:html/parser.dart';
+import 'package:html/dom.dart';
+
+class TormentedRadio {
+  final String _historyUrl = 'http://stream2.mpegradio.com:8070/played.html';
+
+  Future<TormentedRadioHistory> getHistory() async {
+    Response response = await get(_historyUrl);
+
+    if (response.statusCode == 200) {
+      return TormentedRadioHistory.fromDocument(parse(response.body));
+    } else {
+      throw Exception(
+          'Could not get Tormented Radio history: ${response.body}');
+    }
+  }
+}
+
+class TormentedRadioTrack {
+  String name;
+  String artist;
+  DateTime time;
+
+  TormentedRadioTrack({this.name, this.artist, this.time});
+
+  String toString() {
+    return '${this.time.toLocal()} | ${this.name} - ${this.artist}';
+  }
+
+  factory TormentedRadioTrack.fromElement(Element element) {
+    final DateTime now = DateTime.now();
+    final String time = element.nodes[0]?.text;
+    final String fullTitle = element.nodes[1]?.text ?? '';
+
+    // Parse the track's time using the current DateTime as reference
+    // Note: all the times from the history are UTC
+    DateTime trackTime;
+    if (time != null) {
+      List<String> timeParts = time.split(':');
+      trackTime = DateTime(
+          now.year,
+          now.month,
+          now.day,
+          int.parse(timeParts[0]),
+          int.parse(timeParts[1]),
+          int.parse(timeParts[2]),
+          0,
+          0);
+      trackTime = trackTime.add(trackTime.timeZoneOffset);
+
+      if (trackTime.isAfter(now)) {
+        trackTime = trackTime.subtract(Duration(days: 1));
+      }
+    } else {
+      trackTime = now.subtract(Duration());
+    }
+
+    // Some tracks have no title/artist (they're probably jingles)
+    final emptyMatch = RegExp(r'^Empty Title$').firstMatch(fullTitle);
+
+    if (emptyMatch == null) {
+      final match = RegExp(r'^(.*) - (.*)$').firstMatch(fullTitle);
+
+      if (match != null) {
+        return TormentedRadioTrack(
+          name: match.group(2) ?? '',
+          artist: match.group(1) ?? '',
+          time: trackTime,
+        );
+      }
+    }
+
+    return TormentedRadioTrack(
+      name: '',
+      artist: '',
+      time: trackTime,
+    );
+  }
+}
+
+class TormentedRadioHistory {
+  TormentedRadioTrack current;
+  List<TormentedRadioTrack> previous;
+
+  TormentedRadioHistory({this.current, this.previous});
+
+  String toString() {
+    return 'current: ${this.current}, previous: ${this.previous}';
+  }
+
+  factory TormentedRadioHistory.fromDocument(Document document) {
+    // Get all the table rows, minus the header row
+    List<Element> tables = document.getElementsByTagName('table');
+    if (tables.length < 2) return TormentedRadioHistory();
+
+    Element table = tables[1];
+    List<Element> rows = table.getElementsByTagName('tr');
+    rows.removeAt(0);
+
+    if (rows.length == 0) {
+      return TormentedRadioHistory();
+    }
+
+    // Get the current track from the first row and the previous from the others
+    return TormentedRadioHistory(
+      current: TormentedRadioTrack.fromElement(rows[0]),
+      previous: rows
+          .getRange(1, rows.length)
+          .map((Element element) => TormentedRadioTrack.fromElement(element))
+          .toList(),
+    );
+  }
+}
