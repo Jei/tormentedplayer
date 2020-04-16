3ad735411bb2dffed9c04c8a826fd383a7954474
Andrea Jonus <j.jonus.j@gmail.com>
2020-04-16 16:26:58 +1000

    feat: add Preferences class

---
+++ lib/resources/preferences.dart
@@ -0,0 +1,19 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Preferences {
+  final String saveKey;
+
+  Preferences({this.saveKey = 'app'});
+
+  Future<int> loadInt(String valueKey, [int defaultValue]) async {
+    assert(valueKey != null, "A key must be provided");
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getInt('$saveKey.$valueKey') ?? defaultValue;
+  }
+
+  Future<void> saveInt(String valueKey, int value) async {
+    assert(valueKey != null, "A key must be provided");
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setInt('$saveKey.$valueKey', value);
+  }
+}
