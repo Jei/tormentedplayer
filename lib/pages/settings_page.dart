ec08f1620c240b0d65af396e7a28947f6708ebc6
Andrea Jonus <j.jonus.j@gmail.com>
2020-04-23 18:56:03 +1000

    Add SettingsPage

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:preferences/preferences.dart';
+import 'package:provider/provider.dart';
+import 'package:tormentedplayer/models/app_theme_mode.dart';
+
+class SettingsPage extends StatelessWidget {
+  static final String routeName = '/settings';
+
+  @override
+  Widget build(BuildContext context) {
+    AppThemeMode appThemeMode = Provider.of<AppThemeMode>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: PreferencePage([
+        DropdownPreference(
+          'Theme',
+          'theme',
+          defaultVal: ThemeMode.system.index,
+          displayValues: ['Light', 'Dark', 'System default'],
+          values: [
+            ThemeMode.light.index,
+            ThemeMode.dark.index,
+            ThemeMode.system.index
+          ],
+          onChange: appThemeMode.setModeFromIndex,
+        ),
+      ]),
+    );
+  }
+}
