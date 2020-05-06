5a09694d7dc6ac8f4d172fa058e224dd60aa541b
Andrea Jonus <j.jonus.j@gmail.com>
2020-05-06 17:02:23 +1000

    Fix credentials URLs

--- get-credentials.sh
+++ get-credentials.sh
@@ -2,6 +2,6 @@
 
 echo "Retrieving credential files..."
 
-gsutil cp gs://adventures-in-credentials/google-services.json ./android/app/google-services.json
-gsutil cp gs://adventures-in-credentials/keystore.jks ./android/app/keystore.jks
-gsutil cp gs://adventures-in-credentials/keystore.properties ./android/app/keystore.properties
+gsutil cp gs://tormented-player-app-credentials/google-services.json ./android/app/google-services.json
+gsutil cp gs://tormented-player-app-credentials/keystore.jks ./android/app/keystore.jks
+gsutil cp gs://tormented-player-app-credentials/keystore.properties ./android/app/keystore.properties
