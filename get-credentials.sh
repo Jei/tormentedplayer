2675190eff7fedca61d86af81297aeb6be97e4d8
Andrea Jonus <j.jonus.j@gmail.com>
2020-05-06 19:31:55 +1000

    Add Fastlane configuration

--- get-credentials.sh
+++ get-credentials.sh
@@ -5,3 +5,4 @@ echo "Retrieving credential files..."
 gsutil cp gs://tormented-player-app-credentials/google-services.json ./android/app/google-services.json
 gsutil cp gs://tormented-player-app-credentials/keystore.jks ./android/app/keystore.jks
 gsutil cp gs://tormented-player-app-credentials/keystore.properties ./android/app/keystore.properties
+gsutil cp gs://tormented-player-app-credentials/fastlane_service_account.json ./android/fastlane_service_account.json
